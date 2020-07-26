e7673f9c00c3fd8eb3bb0d97efd0986004f2abed
unknown <mail.anandhu@gmail.com>
2020-07-26 23:09:50 +0530

    Updates

---
+++ precache-manifest.6b82bdd7bb4994db62f2c97bd0ffb808.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76ef25b2f68a9cc351734e19c2a4ad1d",
+    "url": "/first_reactProject/index.html"
+  },
+  {
+    "revision": "7258905f26ffbf23ddf3",
+    "url": "/first_reactProject/static/css/main.16782d9d.chunk.css"
+  },
+  {
+    "revision": "e1fd3de284d4bbabccd0",
+    "url": "/first_reactProject/static/js/2.a86485f6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/first_reactProject/static/js/2.a86485f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7258905f26ffbf23ddf3",
+    "url": "/first_reactProject/static/js/main.3535a425.chunk.js"
+  },
+  {
+    "revision": "5bb099b49094c3165962",
+    "url": "/first_reactProject/static/js/runtime-main.0e5eee9e.js"
+  }
+]);
