2b66f75677e807604471fba4ade22b6f61ecf948
codeandcoins <mail.anandhu@gmail.com>
2021-01-23 13:20:51 +0530

    Added debug media link in abouts page

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
 export default function About() {
+
+    const style={color:"white",textAlign:"center",textDecoration:"none"};
+
     return (
         <div>
 
-            <h1 style={{color:"white",textAlign:"center"}}>About</h1>
+            <h1 style={style}>About</h1>
+            <h2 style={style}>React JS Practice Project</h2>
+            <h3 style={style}><a style={style} href="https://youtu.be/DaweP1MTOgs">Click here for tutorial</a></h3>
 
-
-            
         </div>
     )
 }
